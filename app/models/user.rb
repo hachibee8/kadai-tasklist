5e1b9194c431d7cd1f2226b9197826c1e695e101
kakeruhachiya <baby.baby.baby717@gmail.com>
2017-07-04 04:31:10 +0000

    post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  before_save { self.email.downcase! }
+  validates :name,presence: true,length:{maximum: 50}
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: { case_sensitive: false }
+  #Userモデルに[password_digest]カラムを用意し下記のコードを書くと、ログイン認証のための準備をしてくれる。
+  has_secure_password
+  
+  has_many :microposts
 end
